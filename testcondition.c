18d3f84ef8ae2fbfde0d028aacabeabc5d4d8d78
Jean-Christophe-BURNOT <jean-christophe.burnot@cpe.fr>
2022-10-12 18:56:36 +0200

    test cassé

---
+++ testcondition.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * Ce code permet de tester les structures if
+ */
+
+int main()
+{
+	int a=10;
+	if (a>5);
+	{
+		printf("On est passé dans le if");
+	}
+	else if (a>2);
+	{
+		printf("On est passé dans le else if");
+	}
+	else;
+	{
+		printf("On est passé dans le else");
+	}
+	return 0;
+}
